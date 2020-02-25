7988a81e48a5b477f4b3a00a45b4f2d057d9a0a2
DT <44426722+Draganka@users.noreply.github.com>
2020-02-25 13:34:35 +0100

    Update HelloWorld.java

--- src/testPack/HelloWorld.java
+++ src/testPack/HelloWorld.java
@@ -3,7 +3,7 @@ package testPack;
 public class HelloWorld {
 	public static void main(String[] args){
 		//print hello world
-		System.out.println("Hello, World");
+		System.out.println("Hello Dragana");
 	}
 
 }
